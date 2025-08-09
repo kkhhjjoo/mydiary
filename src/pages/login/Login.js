ce459710a601f2821116ae4af62eaead98046571
hyunjoo <khjoo1203@gmail.com>
2025-08-09 17:10:34 +0900

    로그인 구현하기

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import styles from './Login.module.css';
+import { useLogin } from '../../hooks/useLogin';
 
 export default function Login() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {error, isPending, login} = useLogin();
+
     const handleData = (event) => {
         if(event.target.type === "email") {
             setEmail(event.target.value);
@@ -15,7 +18,7 @@ export default function Login() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(email, password);
+        login(email, password);
         
     }
 
@@ -28,8 +31,9 @@ export default function Login() {
 
             <label htmlFor="myPassword">password : </label>
             <input type="password" id="myPassword"  required value={password} onChange={handleData} />
-
-            <button type="submit" className={styles.btn}>로그인</button>
+            {!isPending && <button type="submit" className={styles.btn}>로그인</button>}
+            {isPending && <strong>로그인 진행중입니다...</strong>}
+            {error && <strong>{error}</strong>}
         </fieldset>
        </form>
     )
