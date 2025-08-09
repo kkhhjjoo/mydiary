a766cdcf249caa825dc4c88e86b06963a5029fcb
hyunjoo <khjoo1203@gmail.com>
2025-08-09 14:31:05 +0900

    로그인

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App
